d962035be857876b8c007c4141439068fd1d230d
huymindhack <huyarkadata@gmail.com>
2025-02-25 14:23:23 +0700

    add subheading

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,6 +3,10 @@ export default function Home() {
     <div>
       <section className={"pink_container"}>
           <h1 className={"heading"}>Pitch your starup, <br />connect with entrepreneurs</h1>
+
+          <p className={"sub-heading !max-w-3xl"}>
+            Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competions
+          </p>
       </section>
     </div>
   );
